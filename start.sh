775d4120dae53b5f381045b448891fc1ce0f0fb7
Khải Phan <khaiphan9x@gmail.com>
2020-01-17 03:54:23 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -5,9 +5,10 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 
-kill -9 $(lsof -t -i:8880)
-kill -9 $(lsof -t -i:2052)
-kill -9 $(lsof -t -i:11371)
+kill -9 $(lsof -t -i:8880) > /dev/null 2>&1
+kill -9 $(lsof -t -i:2052) > /dev/null 2>&1
+kill -9 $(lsof -t -i:11371) > /dev/null 2>&1
+kill -9 $(lsof -t -i:9000) > /dev/null 2>&1
 service nginx start
 service php7.3-fpm start
 service php7.3-fpm status
