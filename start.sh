d0a04a91134cca6e29ca33ccc2896ba3c73747f4
Khải Phan <khaiphan9x@gmail.com>
2020-01-17 05:03:06 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 
-service nginx stop > /dev/null 2>&1
-service php7.3-fpm stop > /dev/null 2>&1
+#service nginx stop > /dev/null 2>&1
+#service php7.3-fpm stop > /dev/null 2>&1
 kill -9 $(lsof -t -i:8880) > /dev/null 2>&1
 kill -9 $(lsof -t -i:2052) > /dev/null 2>&1
 kill -9 $(lsof -t -i:11371) > /dev/null 2>&1
