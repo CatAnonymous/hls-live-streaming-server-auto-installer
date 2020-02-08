5eaa2429d218363cf505ba851a057261ef2c237a
Khải Phan <khaiphan9x@gmail.com>
2020-02-09 00:16:41 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -8,9 +8,7 @@ sudo php /root/php-http-proxy-master/start.php stop -d > /dev/null 2>&1
 
 sudo kill -9 $(lsof -t -i:8880) > /dev/null 2>&1
 
-sudo kill -9 $(lsof -t -i:2052) > /dev/null 2>&1
-
-sudo kill -9 $(lsof -t -i:11371) > /dev/null 2>&1
+sudo kill -9 $(lsof -t -i:8686) > /dev/null 2>&1
 
 sudo kill -9 $(lsof -t -i:9000) > /dev/null 2>&1
 
